c81658607c4571b32a4919f4f9c985a7ce7e3ae6
DaveMascia <dave@davemascia.com>
2017-01-30 13:41:23 -0500

    updating api authorization header

--- API_Authorization/index.js
+++ API_Authorization/index.js
@@ -11,7 +11,7 @@ if (process.env.URI === undefined || process.env.APP_NAME === undefined ) {
 const requestOptions = {
   uri: `${process.env.URI}/session`,
   headers: {
-    'Authorization': 'CC_AUTH eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDYXJkQ2FzaCIsInN1YiI6InVybjpDYXJkQ2FzaCIsImF1ZCI6InVybjpDYXJkQ2FzaCIsImlhdCI6MTQ4Mzk5NTg2M30.XXmK9Gsh_hpWb-a2nghszRaMs42iMVFInxuhz0yy41uhoYvesazrrPLph_iQ4p7GtOubzjI9xPv_LWSu2KOZHl1H1tXrG5H6otOhEVWc4zIMk8ncW0GqAEQHHMqjal57Z-T-e1co3lJetO81_B3By4BW1yeUONeuVrVUNFROvcro6REwCm6rxA6VoaCArERJ8S0Wja9m-S6DjNt3MeKA5PoQSirBtBtmwYIBxeNHwBZ7BoP7qbj5jDl-VEWNuz0VnT0fjs2W8siumgZf6e5uqGpTCSbtrvTV11jiHidStjtMmELeRaX_gtD3YX-SJcAI2tHcSF9RH6mejmJt4FGaWQ'
+    'X-CC-APP': process.env.APP_NAME
   },
   method: 'POST',
   resolveWithFullResponse: true
